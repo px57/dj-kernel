fb6d67b2803cd138389659f9bd7d872ced795237
projeta618 <projeta618@gmail.com>
2024-02-16 18:30:14 -0300

    Refactor: Fix unicode issue in admin/link.py and add new functions in docker/ps.py
    Fixed a bug in admin/link.py where a Unicode character was not correctly handled. Also, added two new functions in docker/ps.py to retrieve JSON output of Docker containers and retrieve the list of Docker container names.
    
    Changes:
    - In admin/link.py, fixed Unicode issue and added support for lowercase model paths.
    - In docker/ps.py, added functions dockerPS_to_json() and choice_docker

--- docker/ps.py
+++ docker/ps.py
@@ -1,3 +1,4 @@
+
 import os
 
 def dockerPS_to_json():
@@ -5,3 +6,11 @@ def dockerPS_to_json():
         @description: 
     """
     cmd = "docker ps --format '{{json .}}'"
+    return os.popen(cmd).read()
+
+def choice_docker_container():
+    """
+        @description: 
+    """
+    cmd = "docker ps --format '{{.Names}}'"
+    return os.popen(cmd).read()
