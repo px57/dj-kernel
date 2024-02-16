da4453c1e42f226551e2e1da4dae1f27a39d1362
projeta618 <projeta618@gmail.com>
2024-02-15 21:33:35 -0300

    refactor: Refactoring switcher and stack classes
    The switcher and stack classes have been refactored to improve code readability and maintainability.
    
    Changes:
    - Created a new InterfaceSwitcher class to handle interfaces found in the stack.
    - Added a set_process method to StackSwitcher to define a process for managing the interfaces.
    - Added a has_process method to StackSwitcher to check if a process has been defined.
    - Modified the __getattr__ method in StackSwitcher to utilize

--- interfaces/switcher.py
+++ interfaces/switcher.py
@@ -23,15 +23,55 @@
     Regarder si $INTERFACE_KEY existe, si oui, il va appeler la methode send de chaque interface.
 """
 
+class InterfaceSwitcher:
+    """
+    Il s'agit ici des interfaces qui ont ete trouver dans les stack.
+    """
+    def __init__(self, interfaces):
+        self.interfaces = interfaces
+
+    def __getattr__(self, function_name):
+        """
+            Permet de recuperer une methode d'une interface
+        """
+        def wrapper(*args, **kwargs):
+            for interface in self.interfaces:
+                getattr(interface, function_name)(**kwargs)
+        return wrapper
+
 class StackSwitcher:
     def __init__(self):
+        """
+        Permet de definir un stackSwitcher
+
+        Self 
+            stack_switch: list -> Liste des stack
+            process: function -> Process qui va servir a gerer les differentes interfaces.
+            execute_time: string -> Permet de definir la fonction definit lors de l'appel 
+                                    dynamique, s'executera au sein des interfaces, 
+                                    avant ou apres.
+        """
         self.stack_switch = []
+        self.process = None
+        self.execute_time = 'before' # or after
 
     def load_stack(self, stack):
         """
             Charge un stack
         """
         self.stack_switch.append(stack)
+
+    def set_process(self, process):
+        """
+            Permet de definir un process qui va servir a gerer les differentes interfaces.
+        """
+        self.process = process
+    
+    def has_process(self):
+        """
+            Permet de verifier si un process a ete defini
+        """ 
+        return self.process is not None
     
     def __getattr__(self, function_name):
         """
@@ -44,15 +84,23 @@ class StackSwitcher:
                 if stack.not_has_rule(interface_name):
                     continue
                 callinterface.append(stack.get_rule(interface_name))
+            
+            interfaceSwitcher = InterfaceSwitcher(callinterface)
 
-            for interface in callinterface:
-                getattr(interface, function_name)(**kwargs)
-            return self
+            if self.execute_time == 'before':
+                interfaceSwitcher.__getattr__(function_name)(**kwargs)
+
+            if self.has_process():
+                self.process(interfaceSwitcher, function_name, **kwargs)
+
+            if self.execute_time == 'after':
+                interfaceSwitcher.__getattr__(function_name)(**kwargs)
         return wrapper
     
 res = None
 
 MESSAGE_SWITCHER = StackSwitcher()
+MESSAGE_SWITCHER.set_process(lambda interfaceSwitcher, function_name, **kwargs: print('Process'))
 send = MESSAGE_SWITCHER.send(
     'interfac_key',
     res,
@@ -60,3 +108,5 @@ send = MESSAGE_SWITCHER.send(
         'message': 'Hello World'
     }
 )
+
+
