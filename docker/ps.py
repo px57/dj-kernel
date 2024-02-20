0e4fcd9f8f57926509c62a670f5ff7fa94d834da
projeta618 <projeta618@gmail.com>
2024-02-19 22:00:50 -0300

    chore: Remove unnecessary compiled python files
    These changes involve the removal of compiled Python files (*.pyc) and updating the Docker ps.py script to handle Docker containers and convert them to JSON format.

--- docker/ps.py
+++ docker/ps.py
@@ -1,16 +1,30 @@
 
 import os
+import docker
+import json
 
 def dockerPS_to_json():
     """
         @description: 
     """
-    cmd = "docker ps --format '{{json .}}'"
-    return os.popen(cmd).read()
+    # Connexion au client Docker
+    client = docker.from_env()
+
+    # Récupération des conteneurs en cours d'exécution
+    containers = client.containers.list()
+
+    # Conversion des conteneurs en format JSON
+    json_data = []
+    for container in containers:
+        json_data.append(container.attrs)
+
+    # Affichage du résultat
+    # print(json.dumps(json_data, indent=2))
 
 def choice_docker_container():
     """
         @description: 
     """
-    cmd = "docker ps --format '{{.Names}}'"
-    return os.popen(cmd).read()
+    # cmd = "docker ps --format '{{.Names}}'"
+    # return os.popen(cmd).read()
+    dockerPS_to_json()
