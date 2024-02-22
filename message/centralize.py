f5cd9979e6e46e912c8d3d393db685179ca7d2ca
px57 <david@prepera.io>
2024-02-21 21:32:10 -0300

    Refactor: Remove unused code and add new message processing function
    Why:
    - The code contained unused code related to message switching and sending.
    - It was necessary to refactor the code by removing the unused code and adding a new function for processing messages.
    
    Changes:
    - Removed unused code related to message switching and sending in the switcher.py file.
    - Added a new function process in centralize.py file for running the process of the message.
    - Added a new function switcher_send_message in

--- message/centralize.py
+++ message/centralize.py
@@ -1,4 +1,20 @@
 
 from kernel.interfaces.switcher import StackSwitcher
 
+def process(*args, **kwargs):
+    """
+    Run the process of message. 
+    """
+    print ('##' * 123)
+    print (args)
+    print (kwargs)
+
 MESSAGE_SWITCHER = StackSwitcher()
+MESSAGE_SWITCHER.set_process(process)
+
+def switcher_send_message(*args, **kwargs):
+    """
+    Send message to the message switcher.
+    """
+    MESSAGE_SWITCHER.process(*args, **kwargs)
+
