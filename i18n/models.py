69f36e1dabc4566a3544f052d660238262a157e4
px57 <david@prepera.io>
2024-03-05 17:32:13 -0300

    feat: Add debug print statements and improve language handling in translateDBQuerySet
    Why:
    - Debug print statements have been added for better understanding of the code flow.
    - Improved language handling by setting a default language to 'fr' and retrieving the language from the user's profile if available.
    
    Changes:
    - Added debug print statements in various sections of the translateDBQuerySet function.
    - Set a default language to 'fr' and retrieved the language from the user's profile if available to handle language translation

--- i18n/models.py
+++ i18n/models.py
@@ -59,39 +59,57 @@ def translateDBQuerySet(**kwargs):
     default_params = {
         'autoload_profile': True,
     }
+    print ('################3 translateDBQuerySet 1')
     kwargs = {**default_params, **kwargs}
-        
+    print (kwargs)
+
     request = kwargs.get('request')
     querySet = kwargs.get('querySet')
+    print ('################3 translateDBQuerySet 1.1')
 
     required(request, 'Request is required to translateDBQuerySet')
     required(querySet, 'QuerySet is required to translateDBQuerySet')
 
+    print ('################3 translateDBQuerySet 1.2')
+    language = 'fr'
     if not hasattr(request, 'profile') and kwargs.get('autoload_profile'):
-        request.profile = Profile.objects.get(user=request.user)
-    
-    if request.profile.language == 'en':
+        if request.user.is_authenticated:
+            request.profile = Profile.objects.get(user=request.user)
+            language = request.profile.language
+        elif not request.user.is_authenticated:
+            pass
+            # TOOD: Load the language from the request.
+
+
+    print ('################3 translateDBQuerySet 2')
+    if language == 'en':
         return querySet
 
+    print ('################3 translateDBQuerySet 3')
     query = Q()
     for result in querySet:
         query |= Q(translateObject=result)
  
+    print ('################3 translateDBQuerySet 4')
     if len(querySet) == 0:
         return querySet
     
+    print ('################3 translateDBQuerySet 5')
     if not translation_model_exists(querySet):
         raise Exception('Translation model does not defined to model class ' + getNameOfModelWithQuerySet(querySet))
 
+    print ('################3 translateDBQuerySet 6')
     translation_model = querySet[0].translation_model
     dbTranslate = translation_model.objects.filter(query)
 
+    print ('################3 translateDBQuerySet 7')
     # -> merge the translate with the querySet
     for translate in dbTranslate:
         for result in querySet:
             if result.id == translate.translateObject.id:
                 result._TRANSLATE = translate
 
+    print ('################3 translateDBQuerySet 8')
     return querySet
 
 
