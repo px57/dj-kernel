4cb6c934a9ab368f055ef044be5b3389453dd59b
px57 <david@prepera.io>
2024-03-22 23:21:17 -0300

    feat: Add Redis client class
    Why:
    - This commit adds a new Redis client class to be used for interacting with Redis database.
    
    Changes:
    - Added a new file `redis/__init__.py` importing the `client` module.
    - Added a new file `redis/client.py` containing the implementation of the custom `Redis` class.
    - Defined `__init__` method in the `Redis` class to initialize the Redis client with connection settings from Django settings.
    - Implemented `geoadd

---
+++ redis/client.py
@@ -0,0 +1,70 @@
+
+from django.conf import settings
+
+from redis import Redis 
+
+class Redis(Redis):
+    """
+
+    """
+
+    def __init__(self, cache_name='default'):
+        """
+
+        """
+        CACHE = settings.CACHES[cache_name]
+        super_redis_params = {
+            'host': 'redis',
+            'port': int(CACHE['LOCATION'].split('//')[1].split(':')[1].split('/')[0]),
+            'db': int(CACHE['LOCATION'].split('//')[1].split(':')[1].split('/')[1]),
+        }
+        super(Redis, self).__init__(**super_redis_params)
+
+    def geoadd(self, key, *args, **kwargs):
+        """
+        Add a point to the geospatial index represented by the key.
+
+        Args:
+            key (str): The key of the geospatial index.
+            *args (tuple): The points to add to the geospatial index.
+        """
+        return super(Redis, self).geoadd(key, *args, **kwargs)
+    
+    def geo_nearest_points(self,
+        key, 
+        coords, 
+        radius=1000, 
+        unit='m', 
+        withdist=True
+    ):
+        """
+        Find the nearest points in the radius.
+
+        Args:
+            coords (tuple): The coordinates of the reference point.
+            radius (int): The radius around the reference point.
+            unit (str): The unit of distance.
+        """
+        points = self.georadius(
+            key,
+            coords[0], coords[1],
+            radius, 
+            unit=unit, 
+            withdist=withdist
+        )
+
+        distances = []
+        objects = {}
+
+        # -> find the nearest points, and their distances
+        for point in points:
+            distances.append(point[1])
+            objects[point[1]] = point
+
+        # -> sort the distances
+        distances.sort()
+        return {
+            'id': objects[distances[0]][0],
+            'distance': objects[distances[0]][1],
+            'unit': unit
+        }
